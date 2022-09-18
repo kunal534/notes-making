363f30887b82ac72eb143f9703887040cbb16748
kunal534 <113521848+kunal534@users.noreply.github.com>
2022-09-19 01:46:07 +0530

    notes

---
+++ notes-shower/src/components/Notes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the React Bootcamp</h1>
+      <p>
+        A case study is a research approach that is used to generate an
+        in-depth, multi-faceted understanding of a complex issue in its
+        real-life context. It is an established research design that is used
+        extensively in a wide variety of disciplines, particularly in the social
+        sciences.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
